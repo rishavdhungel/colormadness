2bfbc663b271962671c863ed55ddba878b165bcc
rishavdhungel <rishavdhungel3@gmail.com>
2023-07-24 21:33:55 +0545

    All function Implemented.

--- main.js
+++ main.js
@@ -1,46 +1,196 @@
 const selections = document.querySelectorAll(".optionbtn");
 const mainbtn  = document.querySelector(".mainbtn");
-// console.log(mainbtn);
-// console.log(selections);
-// console.log(selections[0]);
-// console.log(selections[1]);
-// console.log(selections[2]);
+
 let madnessSelection;
 
 for( let i = 0; i < selections.length; i++){
     selections[i].addEventListener('click', function(){
-        console.log(selections[i].getAttribute('data-optionChoice'));
         madnessSelection = selections[i].getAttribute('data-optionChoice');
         mainbtn.setAttribute('onclick',madnessSelection+"()");
     });
     
 }
 
-
-
-
-function madness(){
-    document.body.style.backgroundColor = "rgb(248, 93, 93)";
-    const color = "rgb(248, 93, 93)";
-    document.querySelector(".madnessAnswer").innerText="Background Color:"+`${color}`;
+function rgbavalue(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let transparency = Math.random().toFixed(2);
+    let randomvalue = "rgba("+red+","+green+","+blue+","+transparency+")";
+    return randomvalue;
 }
 
-
 function gradientmadness(){
-    document.body.style.backgroundColor = "red";
-
-
+    let gradient ="linear-gradient(to right,"+rgbavalue()+","+rgbavalue()+"";
+    document.body.style.background = gradient;
+    document.body.style.backgroundColor = rgbavalue();
+    document.querySelector(".madnessAnswer").innerText= "Gradient: "+ gradient;
 }
 
 
 function fixmadness(){
-    document.body.style.backgroundColor = "green";
+    const colornames = ["AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "Darkorange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen"];
+    let randomvalue = colornames[Math.floor(Math.random()*colornames.length)];
+    document.body.style.removeProperty('background');
+    document.body.style.backgroundColor = randomvalue;
+    document.querySelector(".madnessAnswer").innerText = "Background Color: "+randomvalue;
+    document.querySelector(".fixmadness").style.backgroundColor = colornames[Math.floor(Math.random()*colornames.length)];
+    document.querySelector(".infinitemadness").style.backgroundColor = colornames[Math.floor(Math.random()*colornames.length)];
+    document.querySelector(".gradientmadness").style.backgroundColor = colornames[Math.floor(Math.random()*colornames.length)];
+    document.querySelector(".mainbtn").style.backgroundColor = colornames[Math.floor(Math.random()*colornames.length)];
 
-
+    
 }
 
 function infinitemadness(){
-    document.body.style.backgroundColor = "white";
-
-
+    document.body.style.removeProperty('background');
+    let randomvalue = rgbavalue();
+    document.body.style.backgroundColor = randomvalue;
+    document.querySelector(".madnessAnswer").innerText = "Background Color: "+randomvalue;
 }
